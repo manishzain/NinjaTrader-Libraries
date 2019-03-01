be301b253215baa55c31582d04c1126c0c9e2bdf
Tazo Todua <tazotodua@gmail.com>
2019-03-01 18:27:23 -0500

    Update ChartDebuggerWindow.cs

--- Chart-Debugger-Window/AddOns/Library_PuvoxSoftware/ChartDebuggerWindow.cs
+++ Chart-Debugger-Window/AddOns/Library_PuvoxSoftware/ChartDebuggerWindow.cs
@@ -57,9 +57,9 @@ namespace NinjaTrader.NinjaScript.Strategies
 		}
 		
 		// overloads
-		public virtual void PrintBox(object a1, object a2) 							{ PrintBox( (a1==null? "null": a1.ToString()) + " | " +  (a2==null? "null": a2.ToString()) ); }
-		public virtual void PrintBox(object a1, object a2, object a3)				{ PrintBox( (a1==null? "null": a1.ToString()) + " | " +  (a2==null? "null": a2.ToString()) + " | " +  (a3==null? "null": a3.ToString()) );	}
-		public virtual void PrintBox(object a1, object a2, object a3, object a4)	{ PrintBox( (a1==null? "null": a1.ToString()) + " | " +  (a2==null? "null": a2.ToString()) + " | " +  (a3==null? "null": a3.ToString())+ " | " +  (a4==null? "null": a4.ToString()) ); }
+		public virtual void PrintBox(object a1, object a2) 							{ PrintBox( (a1==null? "null": a1.ToString()) + " | " + (a2==null? "null": a2.ToString()) ); }
+		public virtual void PrintBox(object a1, object a2, object a3)				{ PrintBox( (a1==null? "null": a1.ToString()) + " | " + (a2==null? "null": a2.ToString()) + " | " + (a3==null? "null": a3.ToString()) );	}
+		public virtual void PrintBox(object a1, object a2, object a3, object a4)	{ PrintBox( (a1==null? "null": a1.ToString()) + " | " + (a2==null? "null": a2.ToString()) + " | " + (a3==null? "null": a3.ToString())+ " | " + (a4==null? "null": a4.ToString()) ); }
 	
 	}
 }
@@ -85,9 +85,9 @@ namespace NinjaTrader.NinjaScript.Indicators
 		}
 		
 		// overloads
-		public virtual void PrintBox(object a1, object a2) 							{ PrintBox( (a1==null? "null": a1.ToString()) + " | " +  (a2==null? "null": a2.ToString()) ); }
-		public virtual void PrintBox(object a1, object a2, object a3)				{ PrintBox( (a1==null? "null": a1.ToString()) + " | " +  (a2==null? "null": a2.ToString()) + " | " +  (a3==null? "null": a3.ToString()) );	}
-		public virtual void PrintBox(object a1, object a2, object a3, object a4)	{ PrintBox( (a1==null? "null": a1.ToString()) + " | " +  (a2==null? "null": a2.ToString()) + " | " +  (a3==null? "null": a3.ToString())+ " | " +  (a4==null? "null": a4.ToString()) ); }
+		public virtual void PrintBox(object a1, object a2) 							{ PrintBox( (a1==null? "null": a1.ToString()) + " | " + (a2==null? "null": a2.ToString()) ); }
+		public virtual void PrintBox(object a1, object a2, object a3)				{ PrintBox( (a1==null? "null": a1.ToString()) + " | " + (a2==null? "null": a2.ToString()) + " | " + (a3==null? "null": a3.ToString()) );	}
+		public virtual void PrintBox(object a1, object a2, object a3, object a4)	{ PrintBox( (a1==null? "null": a1.ToString()) + " | " + (a2==null? "null": a2.ToString()) + " | " + (a3==null? "null": a3.ToString())+ " | " + (a4==null? "null": a4.ToString()) ); }
 	
 	}
 }
@@ -108,7 +108,7 @@ namespace Library_PuvoxSoftware
 		public NinjaTrader.NinjaScript.Indicators.Indicator indi;
 		public NinjaTrader.NinjaScript.NinjaScriptBase indi_or_strat;
 		
-		private string nl =  Environment.NewLine;
+		private string nl = Environment.NewLine;
 		
 		ChartControl chartControl;
 		ChartPanel chartPnl;
@@ -119,16 +119,16 @@ namespace Library_PuvoxSoftware
 		{
 			if (init && indi_or_strat==null)
 			{
-				indi_or_strat = (NinjaTrader.NinjaScript.NinjaScriptBase)  indi_or_strat_;
+				indi_or_strat = (NinjaTrader.NinjaScript.NinjaScriptBase) indi_or_strat_;
 				 
-				if (indi_or_strat_ is NinjaTrader.NinjaScript.IndicatorBase)  {
+				if (indi_or_strat_ is NinjaTrader.NinjaScript.IndicatorBase) {
 					indi= (NinjaTrader.NinjaScript.Indicators.Indicator) indi_or_strat_;
 					chartControl= indi.ChartControl;
 					chartPnl	= indi.ChartControl.ChartPanels[0];
 					chartBars	= indi.ChartBars;
 				} 
-				else if (indi_or_strat_ is NinjaTrader.NinjaScript.Strategies.Strategy)  {  
-					strat=(NinjaTrader.NinjaScript.Strategies.Strategy)  indi_or_strat_;
+				else if (indi_or_strat_ is NinjaTrader.NinjaScript.Strategies.Strategy) {
+					strat=(NinjaTrader.NinjaScript.Strategies.Strategy) indi_or_strat_;
 					chartControl= strat.ChartControl;
 					chartPnl	= strat.ChartControl.ChartPanels[0];
 					chartBars	= strat.ChartBars;
@@ -144,7 +144,7 @@ namespace Library_PuvoxSoftware
 			else
 			{
 				checkForDeregister();
-			}  
+			}
 		}
 		
 	
@@ -154,20 +154,20 @@ namespace Library_PuvoxSoftware
 			try
 			{
 				chartControl.Dispatcher.InvokeAsync(() =>
-            	{
-					chartPnl.MouseDown +=   this.OnMouseDownEvent ;//new MouseButtonEventHandler(midasMouseEvents);
+				{
+					chartPnl.MouseDown += this.OnMouseDownEvent ;//new MouseButtonEventHandler(midasMouseEvents);
 					chartPnl.KeyDown += this.onKeyDown; 
 					createForm();
 				}); 
 			}
-			catch(Exception e)   { }
+			catch(Exception e){ }
 		}
 		public void deRegisterEventHandlers()
 		{
 			chartPnl.MouseDown -= this.OnMouseDownEvent;
-			chartPnl.KeyDown -= this.onKeyDown;  
+			chartPnl.KeyDown -= this.onKeyDown;
 			if (logForm != null) 
-				logForm.Resize  -= resizeTextarea; 
+				logForm.Resize -= resizeTextarea; 
 			destroyForm(true); 
 		}
 		 
@@ -196,7 +196,7 @@ namespace Library_PuvoxSoftware
 			logForm = new Form();
 			logForm.Width = (lastForm != null ? lastForm.Width : 310);
 			logForm.Height = (lastForm != null ? lastForm.Height : 450);
-			logForm.Resize  += resizeTextarea  ; 
+			logForm.Resize += resizeTextarea; 
 			logForm.Text = @"Debug Print";
 			logForm.TopMost = true; 
 			logForm.Opacity = 0.95; 
@@ -232,27 +232,27 @@ namespace Library_PuvoxSoftware
 		bool isKeyPressed= false;
 		
 		private void onKeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
-			//isKeyPressed =false; if (e.KeyboardDevice.IsKeyDown(Key.LeftShift)) { isKeyPressed =true; lastPressTime=DateTime.Now; }  
+			//isKeyPressed =false; if (e.KeyboardDevice.IsKeyDown(Key.LeftShift)) { isKeyPressed =true; lastPressTime=DateTime.Now; }
 		}
-		//private void onKeyUp(object sender, KeyEventArgs e) { isKeyPressed=false;   Print("keyup");  }
+		//private void onKeyUp(object sender, KeyEventArgs e) { isKeyPressed=false; Print("keyup");}
 		
 		private void OnMouseDownEvent(object sender, MouseButtonEventArgs e)
 		{
 			if (checkForDeregister()) return;
 			
-			Point  cPos = chartControl.MouseDownPoint;
-			int    posX = (int)ChartingExtensions.ConvertToHorizontalPixels(cPos.X-chartControl.BarWidthArray[0], chartControl.PresentationSource);
-			int    slot = (int)chartControl.GetSlotIndexByX(posX);
+			Point cPos = chartControl.MouseDownPoint;
+			int posX = (int)ChartingExtensions.ConvertToHorizontalPixels(cPos.X-chartControl.BarWidthArray[0], chartControl.PresentationSource);
+			int slot = (int)chartControl.GetSlotIndexByX(posX);
 
 			//if clicked on visible
 			if(slot >= chartBars.FromIndex && slot <= chartBars.ToIndex) 
 			{ 
-				int    clicked_Bar = slot;
+				int clicked_Bar = slot;
 
-				if (System.Windows.Forms.Control.ModifierKeys == Keys.Shift)  //isKeyPressed
+				if (System.Windows.Forms.Control.ModifierKeys == Keys.Shift) //isKeyPressed
 				{
 					//isKeyPressed=false;
-					destroyForm(false);  
+					destroyForm(false); 
 					createForm();
 					logForm.Show(); 
 				}
@@ -264,7 +264,7 @@ namespace Library_PuvoxSoftware
 						{ 
 							Bars bars = indi_or_strat.BarsArray[0];
 							
-							int barIdx = indi_or_strat.CurrentBars[0]-clicked_Bar-1;  //indi.CurrentBars[0]
+							int barIdx = indi_or_strat.CurrentBars[0]-clicked_Bar-1; //indi.CurrentBars[0]
 							string bn = Library_PuvoxSoftware.Methods.barIdentifier( indi_or_strat, barIdx );
 							string bn_prev = Library_PuvoxSoftware.Methods.barIdentifier( indi_or_strat, barIdx+1 );
 							//IBar thebar = GetBarFromX(e.X);
@@ -272,12 +272,12 @@ namespace Library_PuvoxSoftware
 							string primary	= ( primaryDict.ContainsKey(bn)	? primaryDict[ bn ]		: "" );
 							/*
 							string secondary= ( secondaryDict.ContainsKey(bn_prev) ? secondaryDict[ bn_prev ]	: "" );
-							string final_text =   (secondary == "" ? "" : secondary + nl +  "<<< BarsInProgress: 0 >>>"+ nl) + primary	;
+							string final_text =  (secondary == "" ? "" : secondary + nl + "<<< BarsInProgress: 0 >>>"+ nl) + primary	;
 							*/
-							string warning_message = nl + "############ Warning ############" +  nl + "The following BIP data is used to form the next Primary BAR."+  nl + "To understand DataSeries better, read: - https://goo.gl/ScpDkN"+nl+"##############################"+nl;
-							string secondary= ( secondaryDict.ContainsKey(bn) ? warning_message +  secondaryDict[bn]	: "" );
+							string warning_message = nl + "############ Warning ############" + nl + "The following BIP data is used to form the next Primary BAR."+ nl + "To understand DataSeries better, read: - https://goo.gl/ScpDkN"+nl+"##############################"+nl;
+							string secondary= ( secondaryDict.ContainsKey(bn) ? warning_message + secondaryDict[bn]	: "" );
 							
-							string final_text =  primary + secondary;
+							string final_text = primary + secondary;
 							textarea.Text = final_text;
 								
 							DrawVerticalLine( bars.GetTime(clicked_Bar+1) );
@@ -289,30 +289,30 @@ namespace Library_PuvoxSoftware
 		 
 		
 		public void PRINT(object text1, object text2) { 
-			PRINT(  (text1 == null ? "null" : (text1 is double ? ((double)text1).ToString("N6") : text1.ToString() )) + " : "+ (text1 == null ? "null" : (text1 is double ? ((double)text1).ToString("N6") : text1.ToString() )) ); 
+			PRINT( (text1 == null ? "null" : (text1 is double ? ((double)text1).ToString("N6") : text1.ToString() )) + " : "+ (text1 == null ? "null" : (text1 is double ? ((double)text1).ToString("N6") : text1.ToString() )) ); 
 		}
 		public void PRINT(object text1)
 		{
 			if (!enabled) return;
 			if (checkForDeregister()) return; 
-			if(indi_or_strat.CurrentBars[0] <1  || indi_or_strat.CurrentBar <1 ) return;
-			string BN_current = Library_PuvoxSoftware.Methods.barIdentifier( indi_or_strat, 0 );  
+			if(indi_or_strat.CurrentBars[0] <1 || indi_or_strat.CurrentBar <1 ) return;
+			string BN_current = Library_PuvoxSoftware.Methods.barIdentifier( indi_or_strat, 0 ); 
 			string currText = ( text1 == null ? "null" : (text1 is double ? ((double)text1).ToString("N6") : text1.ToString() ) ) + nl;
 			
 			if ( indi_or_strat.BarsInProgress == 0 )
 			{
 																			//indi_or_strat.BarsArray[0].GetTime(barNumb)
-				primaryDict[BN_current] = (primaryDict.ContainsKey(BN_current) ? primaryDict[BN_current]  : (indi_or_strat.Times[0][0]).ToString("HH:mm:ss   dd/MMM/yyyy") + " [ BarN: "+indi_or_strat.CurrentBars[0] + "]" + nl + "----------------------------"  + nl  ) ;
+				primaryDict[BN_current] = (primaryDict.ContainsKey(BN_current) ? primaryDict[BN_current] : (indi_or_strat.Times[0][0]).ToString("HH:mm:ss  dd/MMM/yyyy") + " [ BarN: "+indi_or_strat.CurrentBars[0] + "]" + nl + "----------------------------" + nl ) ;
 				primaryDict[BN_current] += currText; 
 			}
 			else
 			{
-				// If this is exected on 2nd timeframe (i.e. from BarsInProgress =1 ), then we should write that for prev.bar, as the bars timestamps are on endings:  https://puvox.software/blog/ninjatrader-multi-timeframe-bevahior-in-realtime-vs-historical/
-				secondaryDict[BN_current] =  (secondaryDict.ContainsKey(BN_current) ? secondaryDict[BN_current]  : "") ;
-				secondaryDict[BN_current] += "•BIP:"+ indi_or_strat.BarsInProgress + " • " + indi_or_strat.Time[0].ToString("dd MMM>HH:mm:ss ") + "PrimaryTF_BN:"+indi_or_strat.CurrentBars[0] + "; ThisTF_BN:"+ indi_or_strat.CurrentBar +" ] " + currText ;
+				// If this is exected on 2nd timeframe (i.e. from BarsInProgress =1 ), then we should write that for prev.bar, as the bars timestamps are on endings: https://puvox.software/blog/ninjatrader-multi-timeframe-bevahior-in-realtime-vs-historical/
+				secondaryDict[BN_current] = (secondaryDict.ContainsKey(BN_current) ? secondaryDict[BN_current] : "") ;
+				secondaryDict[BN_current] += "> BIP:"+ indi_or_strat.BarsInProgress + "•" + indi_or_strat.Time[0].ToString("dd MMM•HH:mm:ss ") + "•Primary_BN:"+indi_or_strat.CurrentBars[0] + "•This_BN:"+ indi_or_strat.CurrentBar +" ]• " + currText ;
 			}
 		}
-   
+  
 		
 		int randLast; 
 		
@@ -327,13 +327,14 @@ namespace Library_PuvoxSoftware
 				// in 5 seconds, remove it
 				System.Threading.Timer timer = null;
 				timer = new System.Threading.Timer(
-				    (object state) => { 
+					(object state) => { 
 						IRB.RemoveDrawObject("Debugger_CurrentBarVLine"+rand);
 						timer.Dispose();
 					}
-				    , null // no state required
-				    ,TimeSpan.FromSeconds(5) // Do it in x seconds
-				    ,TimeSpan.FromMilliseconds(-1)); // don't repeat
+					, null // no state required
+					,TimeSpan.FromSeconds(5) // Do it in x seconds
+					,TimeSpan.FromMilliseconds(-1)
+				); // don't repeat
 				randLast = rand; 
 				
 			}
@@ -344,7 +345,7 @@ namespace Library_PuvoxSoftware
 		}
 		
 		
-	}  
+	} 
 	
 }
 	
@@ -355,28 +356,28 @@ namespace Library_PuvoxSoftware
 #region helpers
 namespace Library_PuvoxSoftware 
 {
-	public partial class Methods   
+	public partial class Methods  
 	{
 		public static string barIdentifier( object NinjaScript_, int idx){
 			NinjaTrader.NinjaScript.NinjaScriptBase obj = (NinjaTrader.NinjaScript.NinjaScriptBase) NinjaScript_;
 			if (obj.CurrentBars[0] <1) return "";
 			return sanitize_filename( getNinjaScriptIdentifier(obj, true) + "--" + obj.BarsArray[0].GetTime(obj.CurrentBars[0]-idx).ToString("yyyy-MM-dd_HH-mm-ss") , "");
-		}  
+		} 
 		
 		public static string getNinjaScriptIdentifier( object NinjaScript_ , bool include_script_name){
 			NinjaTrader.NinjaScript.NinjaScriptBase obj = (NinjaTrader.NinjaScript.NinjaScriptBase) NinjaScript_;
 			return sanitize_filename((include_script_name ? obj.Name + "_" : "" ) + obj.Instrument.FullName + "--" + obj.BarsArray[0].BarsPeriod.BarsPeriodType.ToString() +"-"+obj.BarsArray[0].BarsPeriod.Value.ToString(), "") ; 
 		}
 	
-	    public static string sanitize_filename(string dirtyString, string sanitizeType)
-        {
+		public static string sanitize_filename(string dirtyString, string sanitizeType)
+		{
 			string invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
 			foreach (char c in invalid)
 			{
-			    dirtyString = dirtyString.Replace(c.ToString(), ""); 
+				dirtyString = dirtyString.Replace(c.ToString(), ""); 
 			}
-            return dirtyString;
-        }
+			return dirtyString;
+		}
 	}
 }
 #endregion
@@ -388,7 +389,7 @@ namespace NinjaTrader.NinjaScript.Strategies
 {
 	public partial class Strategy : NinjaTrader.Gui.NinjaScript.StrategyRenderBase
 	{ 
-		public virtual void P(params object[] obj){   PrintBox(obj); }
+		public virtual void P(params object[] obj){ PrintBox(obj); }
 	}
 }
 
@@ -397,7 +398,7 @@ namespace NinjaTrader.NinjaScript.Indicators
 {
 	public partial class Indicator : NinjaTrader.Gui.NinjaScript.IndicatorRenderBase
 	{
-		public virtual void P(params object[] obj){   PrintBox(obj); }
+		public virtual void P(params object[] obj){ PrintBox(obj); }
 	}
 }
 #endregion
